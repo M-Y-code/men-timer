0ae57975732786172378c33021297b3c0106f45f
DESKTOP-NNQKKTB\tokub <tokubou906@yahoo.co.jp>
2023-01-21 06:55:57 +0900

    音テスト

--- components/AsaButton.jsx
+++ components/AsaButton.jsx
@@ -4,7 +4,7 @@ const AsaButton = (props) => {
   return (
     <>
       <button className={styles.btn} onClick={() => props.onClick()}>
-        朝
+        <audio src="../public/sounds/ひらめく2.mp3" />朝
       </button>
     </>
   );
