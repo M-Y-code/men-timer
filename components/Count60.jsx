779a8e4d6519ca6933734bc97eeda769c471247d
DESKTOP-NNQKKTB\tokub <tokubou906@yahoo.co.jp>
2023-01-24 03:50:30 +0900

    safariで音が鳴らない修正テスト4

--- components/Count60.jsx
+++ components/Count60.jsx
@@ -1,8 +1,12 @@
 import sound from "../public/sounds/メッセージ表示音2.mp3";
 const Count60 = () => {
   const Sound = new Audio(sound);
-
-  return Sound.play();
+  Sound.muted = true; // ミュート有効
+  Sound.play(); // 動画再生
+  Sound.pause(); // 動画停止
+  Sound.muted = false; // ミュート無効
+  Sound.currentTime = 0; // 開始秒数を戻す
+  Sound.play();
 };
 
 export default Count60;
